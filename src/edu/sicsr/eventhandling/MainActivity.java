63751dab992cc8787c419f245728339acc1aaedd
rishisicsr <rishi.walani@gmail.com>
2014-12-15 09:47:27 +0530

    button click action

--- src/edu/sicsr/eventhandling/MainActivity.java
+++ src/edu/sicsr/eventhandling/MainActivity.java
@@ -3,21 +3,33 @@ package edu.sicsr.eventhandling;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button btnclick;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnclick = (Button) findViewById(R.id.btnclick);
+        
+        OnClickListener listener =new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				btnclick.setText("Button was clicked");
+				
+				
+			}
+		};
+        
+        btnclick.setOnClickListener(listener);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
     
 }
